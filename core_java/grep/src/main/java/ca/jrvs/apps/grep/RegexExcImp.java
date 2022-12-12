55d6ea1e647c064eda100426e340f8b259d9f7e4
WuWaA <wu.wa.a@qq.com>
2022-12-12 01:09:01 -0500

    Create Java Regex

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/RegexExcImp.java
@@ -0,0 +1,24 @@
+package ca.jrvs.apps.grep;
+
+import ca.jrvs.apps.practice.RegexExc;
+
+public class RegexExcImp implements RegexExc {
+
+    @Override
+    public boolean matchJpeg(String filename) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean matchIp(String ip) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isEmptyLine(String line) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}
