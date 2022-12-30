ca9664ef41c4fd7b3ee78ff67d57d1dd859197bd
Pseudo Wang <wu.wa.a@qq.com>
2022-12-30 03:49:17 -0500

    Restructure Typescript Practice

--- JavaScript_TypeScript/typescript-practice-one/analyzer.js
+++ JavaScript_TypeScript/typescript-practice-one/analyzer.js
@@ -1,47 +1,53 @@
+function analyzeCSS(content, cssRegex, result) {
+    result["contentType"] = "CSS";
+    result["cssTargets"] = {};
+    var cssList = content.match(cssRegex);
+    if (cssList) {
+        cssList.forEach(function (target) {
+            var cssTargetMatch = target.match(/\w*(?=\s*\{)/i);
+            if (cssTargetMatch) {
+                // match "xxx" from "xxx {"
+                var cssTarget = cssTargetMatch[0];
+                if (result["cssTargets"][cssTarget]) {
+                    result["cssTargets"][cssTarget]++;
+                }
+                else {
+                    result["cssTargets"][cssTarget] = 1;
+                }
+            }
+        });
+    }
+}
+function analyzeHTML(content, htmlRegex, result) {
+    result["contentType"] = "HTML";
+    result["tags"] = {};
+    var tagList = content.match(htmlRegex);
+    if (tagList) {
+        // trying to use `map` here
+        tagList.map(function (tag) {
+            // match "xxx" from "<xxx>"
+            var tagNameMatch = tag.match(/(?<=<)\w+(?=>)/i);
+            if (tagNameMatch) {
+                var tagName = tagNameMatch[0];
+                if (result["tags"][tagName]) {
+                    result["tags"][tagName]++;
+                }
+                else {
+                    result["tags"][tagName] = 1;
+                }
+            }
+        });
+    }
+}
 function analyzeContent(content) {
     var result = {};
     var cssRegex = /\w+\s*\{[\w\s#:;]*\}/gi;
     var htmlRegex = /<\w+>/gi;
     if (cssRegex.test(content)) {
-        result["contentType"] = "CSS";
-        result["cssTargets"] = {};
-        var cssList = content.match(cssRegex);
-        if (cssList) {
-            cssList.forEach(function (target) {
-                var cssTargetMatch = target.match(/\w*(?=\s*\{)/i);
-                if (cssTargetMatch) {
-                    // match "xxx" from "xxx {"
-                    var cssTarget = cssTargetMatch[0];
-                    if (result["cssTargets"][cssTarget]) {
-                        result["cssTargets"][cssTarget]++;
-                    }
-                    else {
-                        result["cssTargets"][cssTarget] = 1;
-                    }
-                }
-            });
-        }
+        analyzeCSS(content, cssRegex, result);
     }
     else if (htmlRegex.test(content)) {
-        result["contentType"] = "HTML";
-        result["tags"] = {};
-        var tagList = content.match(htmlRegex);
-        if (tagList) {
-            // trying to use `map` here
-            tagList.map(function (tag) {
-                // match "xxx" from "<xxx>"
-                var tagNameMatch = tag.match(/(?<=<)\w+(?=>)/i);
-                if (tagNameMatch) {
-                    var tagName = tagNameMatch[0];
-                    if (result["tags"][tagName]) {
-                        result["tags"][tagName]++;
-                    }
-                    else {
-                        result["tags"][tagName] = 1;
-                    }
-                }
-            });
-        }
+        analyzeHTML(content, htmlRegex, result);
     }
     else {
         result["contentType"] = "TEXT";
--- JavaScript_TypeScript/typescript-practice-one/analyzer.ts
+++ JavaScript_TypeScript/typescript-practice-one/analyzer.ts
@@ -1,45 +1,53 @@
+function analyzeCSS(content: string, cssRegex: RegExp, result: { [key: string]: any }) {
+  result["contentType"] = "CSS";
+  result["cssTargets"] = {};
+  const cssList = content.match(cssRegex);
+  if (cssList) {
+    cssList.forEach((target) => {
+      const cssTargetMatch = target.match(/\w*(?=\s*\{)/i);
+      if (cssTargetMatch) {
+        // match "xxx" from "xxx {"
+        const cssTarget = cssTargetMatch[0];
+        if (result["cssTargets"][cssTarget]) {
+          result["cssTargets"][cssTarget]++;
+        } else {
+          result["cssTargets"][cssTarget] = 1;
+        }
+      }
+    });
+  }
+}
+
+function analyzeHTML(content: string, htmlRegex: RegExp, result: { [key: string]: any }) {
+  result["contentType"] = "HTML";
+  result["tags"] = {};
+  const tagList = content.match(htmlRegex);
+  if (tagList) {
+    // trying to use `map` here
+    tagList.map((tag) => {
+      // match "xxx" from "<xxx>"
+      const tagNameMatch = tag.match(/(?<=<)\w+(?=>)/i);
+      if (tagNameMatch) {
+        const tagName = tagNameMatch[0];
+        if (result["tags"][tagName]) {
+          result["tags"][tagName]++;
+        } else {
+          result["tags"][tagName] = 1;
+        }
+      }
+    });
+  }
+}
+
 function analyzeContent(content: string): { [key: string]: any } {
   let result: { [key: string]: any } = {};
   const cssRegex = /\w+\s*\{[\w\s#:;]*\}/gi;
   const htmlRegex = /<\w+>/gi;
 
   if (cssRegex.test(content)) {
-    result["contentType"] = "CSS";
-    result["cssTargets"] = {};
-    const cssList = content.match(cssRegex);
-    if (cssList) {
-      cssList.forEach((target) => {
-        const cssTargetMatch = target.match(/\w*(?=\s*\{)/i);
-        if (cssTargetMatch) {
-          // match "xxx" from "xxx {"
-          const cssTarget = cssTargetMatch[0];
-          if (result["cssTargets"][cssTarget]) {
-            result["cssTargets"][cssTarget]++;
-          } else {
-            result["cssTargets"][cssTarget] = 1;
-          }
-        }
-      });
-    }
+    analyzeCSS(content, cssRegex, result);
   } else if (htmlRegex.test(content)) {
-    result["contentType"] = "HTML";
-    result["tags"] = {};
-    const tagList = content.match(htmlRegex);
-    if (tagList) {
-      // trying to use `map` here
-      tagList.map((tag) => {
-        // match "xxx" from "<xxx>"
-        const tagNameMatch = tag.match(/(?<=<)\w+(?=>)/i);
-        if (tagNameMatch) {
-          const tagName = tagNameMatch[0];
-          if (result["tags"][tagName]) {
-            result["tags"][tagName]++;
-          } else {
-            result["tags"][tagName] = 1;
-          }
-        }
-      });
-    }
+    analyzeHTML(content, htmlRegex, result);
   } else {
     result["contentType"] = "TEXT";
     result["lineNumber"] = content.split("\n").length;
