199d3f2bd445fd0f987b1de643bed0b3f3743a56
Pseudo Wang <wu.wa.a@qq.com>
2022-12-29 21:34:00 -0500

    Restructure Javascript Practice

--- JavaScript_TypeScript/javascript-practice-one/analyzer.js
+++ JavaScript_TypeScript/javascript-practice-one/analyzer.js
@@ -1,33 +1,43 @@
+const analyzeCSS = (content, cssRegex, result) => {
+    result.contentType = "CSS";
+    result.cssTargets = {};
+    const cssList = content.match(cssRegex);
+    cssList.forEach(target => {
+        const cssTarget = target.match(/\w*(?=\s*\{)/i)[0]; // match "xxx" from "xxx {"
+        if (result.cssTargets[cssTarget]) {
+            result.cssTargets[cssTarget]++;
+        } else {
+            result.cssTargets[cssTarget] = 1;
+        }
+    });
+    // return result;
+};
+
+const analyzeHTML = (content, htmlRegex, result) => {
+    result.contentType = "HTML";
+    result.tags = {};
+    const tagList = content.match(htmlRegex);
+    tagList.forEach(tag => {
+        const tagName = tag.match(/(?<=<)\w+(?=>)/i)[0]; // match "xxx" from "<xxx>"
+        if (result.tags[tagName]) {
+            result.tags[tagName]++;
+        } else {
+            result.tags[tagName] = 1;
+        }
+    });
+    // return result;
+};
+
 const analyzeContent = content => {
     let result = {};
     const cssRegex = /\w+\s*\{[\w\s#:;]*\}/gi;
     const htmlRegex = /<\w+>/gi;
 
     if (cssRegex.test(content)) {
-        result.contentType = "CSS";
-        result.cssTargets = {};
-        const cssList = content.match(cssRegex);
-        cssList.forEach(target => {
-            const cssTarget = target.match(/\w*(?=\s*\{)/i)[0]; // match "xxx" from "xxx {"
-            if (result.cssTargets[cssTarget]) {
-                result.cssTargets[cssTarget]++;
-            } else {
-                result.cssTargets[cssTarget] = 1;
-            }
-        });
+        analyzeCSS(content, cssRegex, result);
     }
     else if (htmlRegex.test(content)) {
-        result.contentType = "HTML";
-        result.tags = {};
-        const tagList = content.match(htmlRegex);
-        tagList.forEach(tag => {
-            const tagName = tag.match(/(?<=<)\w+(?=>)/i)[0]; // match "xxx" from "<xxx>"
-            if (result.tags[tagName]) {
-                result.tags[tagName]++;
-            } else {
-                result.tags[tagName] = 1;
-            }
-        });
+        analyzeHTML(content, htmlRegex, result);
     }
     else {
         result.contentType = "TEXT";
