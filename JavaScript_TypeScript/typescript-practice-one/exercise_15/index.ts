6b810349dd2df9033f857100082ff66f980aa6f6
Pseudo Wang <wu.wa.a@qq.com>
2023-01-05 20:40:40 -0500

    Update Useful Comments

---
+++ JavaScript_TypeScript/typescript-practice-one/exercise_15/index.ts
@@ -0,0 +1,93 @@
+/*
+
+Intro:
+
+    Our attempt to Open Source didn't work quite as
+    expected. It turned out there were already many
+    existing functional JS libraries.
+
+    All the remaining developers left the company as
+    well. It seems that they are joining a very
+    ambitious startup which re-invented a juicer and
+    raised millions of dollars.
+    Too bad we cannot compete with this kind of
+    financing even though we believe our idea is
+    great.
+
+    It's time to shine for the last time and publish
+    our new invention: object-constructor as our CTO
+    named it. A small library which helps
+    manipulating an object.
+
+Exercise:
+
+    Here is a library which helps manipulating objects.
+    We tried to write type annotations and we failed.
+    Please help!
+
+*/
+
+/*
+export class ObjectManipulator {
+
+    constructor(protected obj) {}
+
+    public set(key, value) {
+        return new ObjectManipulator({...this.obj, [key]: value});
+    }
+
+    public get(key) {
+        return this.obj[key];
+    }
+
+    public delete(key) {
+        const newObj = {...this.obj};
+        delete newObj[key];
+        return new ObjectManipulator(newObj);
+    }
+
+    public getObject() {
+        return this.obj;
+    }
+}
+*/
+
+// computed property syntax
+/*
+const key = 'foo';
+const object = {
+  [key]: 'bar'
+};
+console.log(object.foo); // 'bar'
+*/
+
+// spread syntax
+/*
+const object = { a: 1, b: 2, c: 3 };
+const newObject = { ...object, d: 4 };
+console.log(newObject); // { a: 1, b: 2, c: 3, d: 4 }
+*/
+
+type ObjectWithNewProp<T, K extends string, V> = T & {[NK in K]: V};
+
+export class ObjectManipulator<T> {
+    constructor(protected obj: T) {}
+
+    public set<K extends string, V>(key: K, value: V): ObjectManipulator<ObjectWithNewProp<T, K, V>> {
+        return new ObjectManipulator({...this.obj, [key]: value} as ObjectWithNewProp<T, K, V>);
+    }
+
+    public get<K extends keyof T>(key: K): T[K] {
+        return this.obj[key];
+    }
+
+    public delete<K extends keyof T>(key: K): ObjectManipulator<Omit<T, K>> {
+        const newObj = {...this.obj};
+        delete newObj[key];
+        return new ObjectManipulator(newObj);
+    }
+
+    public getObject(): T {
+        return this.obj;
+    }
+}
