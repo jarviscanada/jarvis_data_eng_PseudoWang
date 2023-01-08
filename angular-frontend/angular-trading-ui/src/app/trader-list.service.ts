ce3e56f5310c4f0467a9a7ecd6795d04e495adb0
Pseudo Wang <wu.wa.a@qq.com>
2023-01-07 19:28:07 -0500

    Fix Mistake

--- angular-frontend/angular-trading-ui/src/app/trader-list.service.ts
+++ angular-frontend/angular-trading-ui/src/app/trader-list.service.ts
@@ -1,9 +1,47 @@
+import { Trader } from './trader';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TraderListService {
-
-  constructor() { }
+  traderList: Trader[] = [
+    {
+      key: '1',
+      id: 1,
+      firstName: 'Mike',
+      lastName: 'Spencer',
+      dob: new Date().toLocaleDateString,
+      country: 'Canada',
+      email: 'mike@email.com',
+      amount: 0,
+      actions: `<button (click)="deleteTrader">Delete Trader</button>`,
+    },
+    {
+      key: '2',
+      id: 2,
+      firstName: 'Hellen',
+      lastName: 'Miller',
+      dob: new Date().toLocaleDateString(),
+      country: 'USA',
+      email: 'hellen@email.com',
+      amount: 0,
+      actions: `<button (click)="deleteTrader">Delete Trader</button>`,
+    },
+  ];
+  constructor() {}
+  getDataSource(): Observable<Trader[]> {
+    return of(this.traderList);
+  }
+  getColumns(): string[] {
+    return [
+      'First Name',
+      'Last Name',
+      'Email',
+      'DateOfBirth',
+      'Country',
+      'Actions',
+    ];
+  }
 }
