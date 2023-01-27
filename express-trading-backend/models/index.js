3de2621a98e8e5a0b31b899caf058449172eb6d3
Pseudo Wang <wu.wa.a@qq.com>
2023-01-26 22:53:11 -0500

    Update Express

--- express-trading-backend/models/index.js
+++ express-trading-backend/models/index.js
@@ -1,3 +1,7 @@
 const Quote = require('./Quote');
+const Trader = require('./Trader');
+const Transaction = require('./Transaction');
 
 module.exports = { Quote };
+module.exports = { Trader };
+module.exports = { Transaction };
