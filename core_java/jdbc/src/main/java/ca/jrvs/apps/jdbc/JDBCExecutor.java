e4600a2571d7393e72605ac25b667a377047f7e6
Pseudo Wang <wu.wa.a@qq.com>
2023-02-11 02:09:03 -0500

    Update JDBC

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -4,19 +4,26 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Logger;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class JDBCExecutor {
-    private static Logger logger = Logger.getLogger(JDBCExecutor.class.getName());
+    private static final Logger logger = LogManager.getLogger(JDBCExecutor.class);
 
     public static void main(String[] args) {
         DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost", "postgres", "postgres", "password");
         try {
             Connection connection = dcm.getConnection();
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM sql_features LIMIT 10;");
-            while (resultSet.next())
-                logger.info(resultSet.getString("feature_name"));
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM information_schema.sql_parts;");
+            while (resultSet.next()) {
+                logger.info(resultSet.getString(1));
+                logger.info(resultSet.getString(2));
+                logger.info(resultSet.getString(3));
+                System.out.println(resultSet.getString(1));
+                System.out.println(resultSet.getString(2));
+                System.out.println(resultSet.getString(3));
+            }
             statement.close();
             connection.close();
         } catch (SQLException e) {
