9c131d3d12e7371b6e2bd4163dcad9a636656e41
Pseudo Wang <wu.wa.a@qq.com>
2023-02-23 05:29:31 -0500

    Implementing TwitterController

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/controller/Controller.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/controller/Controller.java
@@ -1,5 +1,6 @@
 package ca.jrvs.apps.twitter.controller;
 
+import ca.jrvs.apps.twitter.dao.helper.TwitterRuntimeException;
 import ca.jrvs.apps.twitter.model.Tweet;
 import java.util.List;
 
@@ -10,25 +11,25 @@ public interface Controller {
    *
    * @param args
    * @return a posted tweet
-   * @throws IllegalArgumentException if args are invalid
+   * @throws TwitterRuntimeException if args are invalid
    */
-  Tweet postTweet(String[] args);
+  Tweet postTweet(String[] args) throws TwitterRuntimeException;
 
   /**
    * Parse user argument and search a tweet by calling service classes
    *
    * @param args
    * @return a tweet
-   * @throws IllegalArgumentException if args are invalid
+   * @throws TwitterRuntimeException if args are invalid
    */
-  Tweet showTweet(String[] args);
+  Tweet showTweet(String[] args) throws TwitterRuntimeException;
 
   /**
    * Parse user argument and delete tweets by calling service classes
    *
    * @param args
    * @return a list of deleted tweets
-   * @throws IllegalArgumentException if args are invalid
+   * @throws TwitterRuntimeException if args are invalid
    */
-  List<Tweet> deleteTweet(String[] args);
+  List<Tweet> deleteTweet(String[] args) throws TwitterRuntimeException;
 }
---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/controller/TwitterController.java
@@ -0,0 +1,63 @@
+package ca.jrvs.apps.twitter.controller;
+
+import ca.jrvs.apps.twitter.dao.helper.TwitterRuntimeException;
+import ca.jrvs.apps.twitter.model.Coordinates;
+import ca.jrvs.apps.twitter.model.Tweet;
+import ca.jrvs.apps.twitter.service.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.LinkedList;
+import java.util.List;
+
+@org.springframework.stereotype.Controller
+public class TwitterController implements Controller {
+    private static final String COORD_SEP = ":";
+    private static final String COMMA = ",";
+    Service service;
+
+    @Autowired
+    public TwitterController(Service service) {
+        this.service = service;
+    }
+
+    @Override
+    public Tweet postTweet(String[] args) throws TwitterRuntimeException {
+        if (args.length != 3)
+            throw new TwitterRuntimeException("USAGE: post \"tweet text\" \"long:lat\"");
+
+        String text = args[1];
+        String coord = args[2];
+        String[] coordArray = coord.split(COORD_SEP);
+        Double longitude = Double.parseDouble(coordArray[0]);
+        Double latitude = Double.parseDouble(coordArray[1]);
+        Tweet tweet = new Tweet();
+        Coordinates coordinates = new Coordinates();
+        List<Double> coordinate = new LinkedList<>();
+
+        coordinate.add(longitude);
+        coordinate.add(latitude);
+        coordinates.setCoordinates(coordinate);
+        tweet.setText(text);
+        tweet.setCoordinates(coordinates);
+
+        return service.postTweet(tweet);
+    }
+
+    @Override
+    public Tweet showTweet(String[] args) throws TwitterRuntimeException {
+        if (args.length != 2)
+            throw new TwitterRuntimeException("USAGE: show \"TweetID\" ");
+
+        String tweetID = args[1];
+        return service.showTweet(tweetID, null);
+    }
+
+    @Override
+    public List<Tweet> deleteTweet(String[] args) throws TwitterRuntimeException {
+        if (args.length != 2)
+            throw new TwitterRuntimeException("USAGE: delete \"TweetID1,TweetID2,TweetID3\"");
+
+        String tweetIDs = args[1];
+        String[] tweetIDArray = tweetIDs.split(COMMA);
+        return service.deleteTweets(tweetIDArray);
+    }
+}
